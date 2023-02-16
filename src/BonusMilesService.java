c1755464600186dfadf4285783e7f8677c9cebdc
KOTE <koss85@gmail.com>
2023-02-16 22:19:52 +0300

    src add

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int miles;
-        int bonusmiles = 20; // стоимость бонусной мили.
-        miles = price / bonusmiles; // количество бонусных миль за покупку билета.
+        int bonusMiles = 20; // стоимость бонусной мили.
+        miles = price / bonusMiles; // количество бонусных миль за покупку билета.
         return miles;
     }
 
 
-    }
+}
